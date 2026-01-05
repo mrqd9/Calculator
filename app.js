8966f5c85d4dd3c10651001dd7639a652cc7c526
mrqd9 <56225476+mrqd9@users.noreply.github.com>
2026-01-05 15:54:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/*
+ * Adding Calculator Pro
+ * Copyright (C) 2026 mrqd9
+ * * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 let historyEl = document.getElementById("history");
 let liveEl = document.getElementById("live");
 let totalEl = document.getElementById("total");
